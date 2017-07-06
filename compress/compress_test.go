7c6d75791083391585ac116aefc82b51751fa25d
tangzhengchao <za-tangzhengchao@zhongan.io>
2017-07-06 20:53:12 +0800

    add compress rate calculate

--- compress/compress_test.go
+++ compress/compress_test.go
@@ -3,29 +3,51 @@ package compress
 //main to test rate of comress, speed of compress and decompress
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
 func BenchmarkZipCompress(b *testing.B) {
-	ZipCompress()
+	os.Remove("cailiao.zip")
+	for i := 0; i < b.N; i++ {
+		ZipCompress()
+	}
+	zipCompressRate()
 }
 
-func BenchmarkSnappyCompress(b *testing.B) {
+// func BenchmarkLZ4Compress(b *testing.B) {
+//
+// }
+//
+// func BenchmarkZipDecompress(b *testing.B) {
+//
+// }
+//
+// func BenchmarkLZ4Decompress(b *testing.B) {
+//
+// }
 
+func getDirSize(dir string) int64 {
+	fs, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Println(err)
+		return 0
+	}
+	var size int64
+	for _, file := range fs {
+		size += file.Size()
+	}
+	return size
 }
 
-func BenchmarkLZ4Compress(b *testing.B) {
-
-}
-
-func BenchmarkZipDecompress(b *testing.B) {
-
-}
-
-func BenchmarkSnappyDecompress(b *testing.B) {
-
-}
-
-func BenchmarkLZ4Decompress(b *testing.B) {
-
+func zipCompressRate() {
+	dirsize := getDirSize(dir)
+	info, err := os.Stat("cailiao.zip")
+	if err != nil {
+		fmt.Println(err)
+	}
+	rate := float64(info.Size()) / float64(dirsize)
+	fmt.Println("zip compress rate >>>>>", rate*100)
 }
